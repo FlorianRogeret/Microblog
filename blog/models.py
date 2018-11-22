8f891c31bf10504250c8f62ca06b169cc1c25672
FlorianRogeret <florian.rogeret@outlook.fr>
2018-11-22 18:00:30 +0100

    Debut de création dela base de donnée

--- blog/models.py
+++ blog/models.py
@@ -4,3 +4,15 @@ from __future__ import unicode_literals
 from django.db import models
 
 # Create your models here.
+
+class User(models.Model):
+    username = models.CharField(max_length=20)
+    first_name = models.CharField(max_length=20)
+    last_name = models.CharField(max_length=20)
+    email = models.CharField(max_length=100)
+
+class Post(models.Model):
+    title = models.CharField(max_length=100)
+    body = models.CharField(max_length=500)
+    creation_date = models.DateTimeField('date published')
+    update_date = models.DateTimeField('date update')
