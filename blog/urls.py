31de875f85d262714b0767448aba24bbd78773a9
Florian Rogeret <florian.rogeret@outlook.fr>
2018-12-11 10:46:26 +0100

    Ajout CSS

--- blog/urls.py
+++ blog/urls.py
@@ -10,13 +10,13 @@ urlpatterns = [
     path('createarticle/',views.article_create, name='create'),
     #page that redirect to a specif article
     path('articledetail/<int:id>/',views.article_detail, name='detail'),
-
+    #Page that allow to site a user page 
     path('authorarticle/<str:username>/',views.article_user, name='article_user'),
-
+    #Page that allow to delet an article
     path('deletearticle/<int:id>/',views.delete_article, name='delete'),
-
+    #Page that allow to update an article
     path('updatearticle/<int:id>/',views.article_update, name='modifie'),
-
+    #View that allow to apply the modification of the article
     path('doupdatearticle/<int:id>/',views.article_doupdate, name='domodifie'),
 ]
 
